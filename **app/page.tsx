7648b846968a426e1e3bb2b7ad03296242c22e1a
MaiPspace <syowa.kirakira@gmail.com>
2025-04-27 20:36:14 +0900

    Delete **app directory

--- **app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold mb-6 text-center">Discord ロール分析ツール</h1>
-      <p className="text-center">設定中です。しばらくお待ちください。</p>
-    </main>
-  )
-}
